9b8fe6c6647a7ceb89decb4e202c0530bb36865d
Roberth Riera <riera_1992@hotmail.com>
2018-09-20 01:04:05 -0430

    Avance de envios de correo - Ajustes

--- app/Facades/Mail/NotificationMail.php
+++ app/Facades/Mail/NotificationMail.php
@@ -10,12 +10,188 @@ use App\Models\LogMail;
 use App\Models\Module;
 use Route;
 use Exception;
+use Illuminate\Database\Eloquent\Collection;
+use App\User;
 
 class NotificationMail
 {
+    /**
+     * Recipients to whom the mail will be sent
+     *
+     * @var Illuminate\Database\Eloquent\Collection
+     * @var App\User
+     */
+    private $recipients;
+
+    /**
+     * Subject of the mail
+     *
+     * @var string
+     */
+    private $subject;
+
+    /**
+     * Mail message
+     *
+     * @var string
+     */
+    private $message;
+
+    /**
+     * Name of the view that will be used in the mail content
+     *
+     * @var string
+     */
+    private $view;
+
+    /**
+     * Arrangement of buttons
+     *
+     * @var array
+     */
+    private $buttons = [];
+
+    /**
+     * Stores a list
+     *
+     * @var array
+     */
+    private $list;
+
+    private $table;
+
+    /**
+     * Module that executes the event
+     *
+     * @var App\Models\Module
+     */
+    private $module;
+
     public function __construct()
     {
+ 
+    }
 
+    /**
+     * Edit the recipients of the email
+     *
+     * @param Illuminate\Database\Eloquent\Collection $recipients
+     * @param App\User $recipients
+     */
+
+    public function recipients($recipients)
+    {
+        if (!$recipients instanceof Collection && !$recipients instanceof User)
+            throw new \Exception('Invalid recipient format');
+        
+        $this->recipients = $recipients;
+
+        return $this;
+    }
+
+    /**
+     * Edit the mail subject
+     *
+     * @param string $subject
+     */
+    public function subject($subject)
+    {
+        if (!is_string($subject) || $subject == '')
+            throw new \Exception('The format of the subject is incorrect'); 
+
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    /**
+     * Edit the mail message
+     *
+     * @param string $message
+     */
+    public function message($message)
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+    
+    /**
+     * Edit the view that will be used in the mail content
+     *
+     * @param string $view
+     */
+    public function view($view)
+    {
+        if (!is_string($view) || $view == '')
+            throw new \Exception('The format of the view is incorrect'); 
+
+        $this->view = $view;
+
+        return $this;
+    }
+    
+    /**
+     * Edit the buttons that will be available in the mail view
+     *
+     * @param array $buttons
+     */
+    public function buttons($buttons)
+    {   
+        if (!is_array($buttons))
+            throw new \Exception('The array of buttons does not comply with the format: array (array ("text" => "Download", "url" => "www.example.com"))');
+
+        foreach($buttons as $button)
+        {
+            if (!isset($button["text"]) || !isset($button["url"]))
+                throw new \Exception('The array of buttons does not comply with the format: array (array ("text" => "Download", "url" => "www.example.com"))');
+        }
+
+        $this->buttons = $buttons;
+
+        return $this;
+    }
+    
+    /**
+     * Edit the list that will be displayed in the mail
+     *
+     * @param array $data
+     */
+    public function list($data)
+    {
+        if (!is_array($data))
+            throw new \Exception('The format of the list is incorrect');
+
+        $this->list = $data;
+
+        return $this;
+    }
+
+    /**
+     * Edit the module that performs the action
+     *
+     * @param App\Models\Module $module
+     */
+    public function module($module)
+    {
+        if (!$module instanceof Module)
+            throw new \Exception('Invalid module format');
+        
+        $this->module = $module;
+
+        return $this;
+    }
+
+    /**
+     * Send the mail
+     *
+     */
+    public function send()
+    {
+        if ($this->recipients instanceof Collection && $this->recipients->isEmpty())
+            throw new \Exception(trans('mail.recipient_empty'));
+        else if (!$this->recipients instanceof User)
+            throw new \Exception(trans('mail.recipient_empty'));
     }
 
     public static function sendMail(MailInformation $mail)
@@ -51,6 +227,7 @@ class NotificationMail
             $log->save();
         }
         catch (\Exception $e) {
+            dd($e);
             throw new \Exception(trans('mail.send_error'));
         }
     }
--- app/Http/Controllers/EmailTestController.php
+++ app/Http/Controllers/EmailTestController.php
@@ -3,20 +3,50 @@
 namespace App\Http\Controllers;
 use App\Models\MailInformation;
 use App\Facades\Mail\Facades\NotificationMail;
+use App\User;
+use App\Models\Module;
 
 class EmailTestController extends Controller
 {
     public function index()
-    {
-        $mail = new MailInformation();
-        $mail->setRecipients(['riera_1992@hotmail.com']);
+    {   
+        //$users = User::find(2);
+        $users = User::get();
+        //$users = ['riera_1992@hotmail.com'];
+        //dd($users);
+        
+        /*$mail = new MailInformation();
+        $mail->setRecipients($users);
         $mail->setMessage("Este es un mensaje de prueba");
         $mail->setModule(1);
         //$mail->setFrom(['address'=>'sause@gmail.com', 'name'=>'Roberth Riera']);
         $mail->setFrom('sause@gmail.com');
         //$mail->setSubject('Notification Test');
 
-        return NotificationMail::sendMail($mail);
+        return NotificationMail::sendMail($mail);*/
+
+        $buttons = [
+            ['text'=>'Descargar', 'url'=>'www.example.com'],
+            ['text'=>'Oferta', 'url'=>'www.oferta.com'],
+        ];
+
+        $list = [
+            'Primer elemento de la lista',
+            'Segundo elemanto de la lista',
+            'Tercer elemento de la lista'
+        ];
+
+        $module = Module::where('name', 'mod 1')->first();
+        
+        dd(NotificationMail::view('asd')
+            ->subject('Notificacion')
+            ->message('Mensaje de prueba')
+            ->recipients($users)
+            ->buttons($buttons)
+            ->list($list)
+            ->module($module)
+            ->send()
+        );
 
     }
 }
