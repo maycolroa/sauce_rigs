2a4c168260f160c1d8902f0b742db781846c4722
Roberth Riera <riera_1992@hotmail.com>
2018-09-24 22:56:12 -0430

    Migraciones y seeder de aplicaciones y modulos

---
+++ database/seeds/ApplicationsSeeder.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Application;
+
+class ApplicationsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::beginTransaction();
+
+        try
+        {   
+            $this->command->info('Comienza el seeder de las aplicaciones');
+
+            $file = dirname(__FILE__) . '/data/applications.json';
+            $file = file_get_contents($file);
+            $applications = json_decode($file, true);
+
+            foreach ($applications as $value)
+            {	
+                if (isset($value['name']) && isset($value['display_name']) && isset($value['image']))
+                {
+                    $item = [];
+                    $app = Application::where("name", $value["name"])->first(); 
+                    
+                    if (!$app)
+                    {
+                        $item = [
+                            "name" 	        => $value["name"],
+                            "display_name" 	=> $value["display_name"],
+                            "image" 		=> $value["image"],
+                            "created_at" 	=> date("Y-m-d H:i:s")
+                        ];
+
+                        if (isset($value["id"]))
+                            $item["id"] = $value["id"];
+
+                        Application::create($item);
+                    }
+                    else
+                        $this->command->info("Elemento ".$value["name"]." duplicado");
+                }
+                else
+                {
+                    $this->command->info('Elemento omitido por formato invalido: '. json_encode($value));
+                }
+			}
+
+            DB::commit();
+            $this->command->info('Proceso terminado exitosamente');
+
+        } catch (\Exception $e) {
+            DB::rollback();
+            $this->command->info('Ocurrio un error al ejecutar la clase ApplicationsSeeder');
+        }
+    }
+}
---
+++ database/seeds/ModulesSeeder.php
@@ -0,0 +1,78 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Application;
+use App\Models\Module;
+
+class ModulesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::beginTransaction();
+
+        try
+        {   
+            $this->command->info('Comienza el seeder de los modulos');
+
+            $file = dirname(__FILE__) . '/data/modules.json';
+            $file = file_get_contents($file);
+            $modules = json_decode($file, true);
+
+            foreach ($modules as $value)
+            {	
+                if (isset($value['application']) && isset($value['modules']))
+                {
+                    $app = Application::where("name", $value["application"])->first();
+                    $data = [];
+
+                    if ($app)
+                    {
+                        foreach ($value["modules"] as $item)
+                        {
+                            if (isset($item["name"]) && isset($item["display_name"]))
+                            {
+                                $mod = Module::where("application_id", $app->id)
+                                        ->where("name", $item["name"])->first(); 
+
+                                if (!$mod)
+                                {
+                                    $data = [
+                                        "name" 	         => $item["name"],
+                                        "display_name" 	 => $item["display_name"],
+                                        "application_id" => $app->id,
+                                        "created_at" 	 => date("Y-m-d H:i:s")
+                                    ];
+                
+                                    Module::create($data);
+                                }
+                                else 
+                                    $this->command->info("Elemento ".$item["name"]." duplicado");
+                            }
+                            else
+                                $this->command->info('Elemento omitido por formato invalido: '. json_encode($item));
+                        }
+                    }
+                    else
+                        $this->command->info("Aplicacion ".$value["application"]." no encontrada");
+                }
+                else
+                {
+                    $this->command->info('Elemento omitido por formato invalido: '. json_encode($value));
+                }
+			}
+
+            DB::commit();
+            $this->command->info('Proceso terminado exitosamente');
+
+        } catch (\Exception $e) {
+            dd($e);
+            DB::rollback();
+            $this->command->info('Ocurrio un error al ejecutar la clase ModulesSeeder');
+        }
+    }
+}
