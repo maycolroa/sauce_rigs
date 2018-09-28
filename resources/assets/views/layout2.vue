dfca81d706df11938b847deb33e2dd91f57696f0
anderssonflorez <florezanderson@thotstrategy.com>
2018-09-28 16:18:08 -0500

    Ajustes manu

--- resources/assets/views/layout2.vue
+++ resources/assets/views/layout2.vue
@@ -1,19 +1,7 @@
 <template>
-<div class="layout-wrapper layout-2">
-  <div class="layout-inner">
-    <div class="layout-container">
-      <Navbar :data="data" :appSelected="appSelected" @changeApp="changeModules($event)"/>
-
-      <div class="layout-content">
-        <div class="router-transitions container-fluid flex-grow-1 container-p-y">
-          <router-view />
-        </div>
-      </div>
-    </div>
-    
-  </div>
-  <div class="layout-overlay" @click="closeSidenav"></div>
-  <notifications group="app"/>
+<div>
+  <sidenav :data="data" :appSelected="appSelected"/>
+  <router-view />
 </div>
 </template>
 <script>
@@ -55,10 +43,6 @@ export default {
                 Alerts.error('Error', 'Se ha generado un error en el proceso, por favor contacte con el administrador');
             });
       },
-      changeModules(event)
-      {
-        this.appSelected = event
-      }
     }
 }
 </script>
