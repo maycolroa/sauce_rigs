7f74baf5dcc7801a471138207ecdf43dcf8651de
Roberth Riera <robertjoserieraumbria@gmail.com>
2019-04-04 09:38:45 -0400

    Evaluaciones - adelanto

--- app/LegalAspects/ContractLessee.php
+++ app/LegalAspects/ContractLessee.php
@@ -12,16 +12,22 @@ class ContractLessee extends Model
     protected $table = 'sau_ct_information_contract_lessee';
 
     protected $fillable = [
+        'company_id',
         'nit',
+        'classification',
         'type',
         'business_name',
         'phone',
         'address',
         'legal_representative_name',
+        'environmental_management_name',
+        'economic_activity_of_company',
+        'arl',
         'SG_SST_name',
+        'risk_class',
         'number_workers',
         'high_risk_work',
-        'social_reason'
+        'social_reason',
     ];
 
     public function multiselect()
