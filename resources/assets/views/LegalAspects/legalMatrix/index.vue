c58c539f573adc34c5cf8250b1c318046e4bc508
Roberth Riera <robertjoserieraumbria@gmail.com>
2019-06-05 22:54:57 -0400

    Add icon

--- resources/assets/views/LegalAspects/legalMatrix/index.vue
+++ resources/assets/views/LegalAspects/legalMatrix/index.vue
@@ -64,7 +64,7 @@
 
 							<div class="d-flex col-sm-6 col-md-4 col-lg-6 align-items-center">
 								<router-link tag="a" href="javascript:void(0)" class="card-body media align-items-center text-dark" :to="{name:'legalaspects-lm-law'}">
-									<i class="lnr lnr-user display-4 d-block text-primary"></i>
+									<i class="lnr lnr-layers display-4 d-block text-primary"></i>
 									<span class="media-body d-block ml-3">
 										<span class="text-big font-weight-bolder">Administrar Normas</span>
 										<br>
