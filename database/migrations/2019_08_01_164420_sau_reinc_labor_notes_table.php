e6a0e7e413ceda7e95e69e9fb3a997f4d2fe1c54
Deangerlin Loaiza <yelinloaiza@gmail.com>
2019-08-01 18:09:21 -0400

    SAUC-328 #comment tablas necesarias para reincorporaciones

---
+++ database/migrations/2019_08_01_164420_sau_reinc_labor_notes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class SauReincLaborNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sau_reinc_labor_notes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('description');
+            $table->unsignedInteger('check_id');
+            $table->unsignedInteger('user_id');
+            $table->timestamps();
+
+            $table->foreign('check_id')->references('id')->on('sau_reinc_checks')
+                ->onUpdate('cascade')->onDelete('cascade');
+            $table->foreign('user_id')->references('id')->on('sau_users')
+                ->onUpdate('cascade')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sau_reinc_labor_notes');
+    }
+}
