ae91740525c151ab7a45b4ddfaa13519c2582224
Deangerlin Loaiza <yelinloaiza@gmail.com>
2020-03-19 12:03:23 -0400

    SAUC-524 #commet Visualizar los roles definidos de los modulos a los cuales se tiene acceso segun la licencia activa de la compañia del usuario

--- app/Models/Administrative/Roles/Role.php
+++ app/Models/Administrative/Roles/Role.php
@@ -5,6 +5,7 @@ namespace App\Models\Administrative\Roles;
 use Laratrust\Models\LaratrustRole;
 //use App\Traits\RoleCompanyTrait;
 use Illuminate\Support\Facades\Auth;
+use App\Models\System\Licenses\License;
 use Session;
 
 class Role extends LaratrustRole
@@ -34,29 +35,18 @@ class Role extends LaratrustRole
         return $this->belongsTo('App\Models\General\Module', 'module_id');
     }
 
-    /*public function scopeConditionController($query)
-    {
-        $query->withoutGlobalScopes();
-
-        if (Auth::user()->hasPermission('roles_manage_defined'))
-          $query->whereRaw('(sau_roles.company_id = '.Session::get('company_id').' OR sau_roles.company_id IS NULL)');
-        else
-          $query->where('sau_roles.company_id', Session::get('company_id'));
-
-        return $query;
-    }
-
-    public function scopeConditionGeneral($query)
-    {
-      $query->withoutGlobalScopes();
-
-      $query->where('sau_roles.company_id', Session::get('company_id'));
-    }*/
-
     public function scopeForm($query, $includeSuper = false, $company = null)
     {
       $company = $company ? $company : Session::get('company_id');
-      $query->whereRaw("sau_roles.company_id = {$company} OR (sau_roles.company_id IS NULL AND sau_roles.name NOT IN('Contratista', 'Arrendatario'))");
+
+      $modules = License::
+              join('sau_license_module', 'sau_license_module.license_id', 'sau_licenses.id')
+            ->whereRaw('? BETWEEN started_at AND ended_at', [date('Y-m-d')]);
+
+      $modules->company_scope = $company;
+      $modules = $modules->pluck('sau_license_module.module_id')->unique()->implode(",");
+
+      $query->whereRaw("sau_roles.company_id = {$company} OR (sau_roles.company_id IS NULL AND sau_roles.name NOT IN('Contratista', 'Arrendatario') AND module_id IN ({$modules}))");
 
       if (!$includeSuper)
         $query->where('sau_roles.name', '<>', 'Superadmin');
@@ -67,7 +57,15 @@ class Role extends LaratrustRole
     public function scopeAlls($query, $includeSuper = false, $company = null)
     {
       $company = $company ? $company : Session::get('company_id');
-      $query->whereRaw("sau_roles.company_id = {$company} OR sau_roles.company_id IS NULL");
+
+      $modules = License::
+              join('sau_license_module', 'sau_license_module.license_id', 'sau_licenses.id')
+            ->whereRaw('? BETWEEN started_at AND ended_at', [date('Y-m-d')]);
+
+      $modules->company_scope = $company;
+      $modules = $modules->pluck('sau_license_module.module_id')->unique()->implode(",");
+
+      $query->whereRaw("sau_roles.company_id = {$company} OR (sau_roles.company_id IS NULL AND module_id IN ({$modules}))");
 
       if (!$includeSuper)
         $query->where('sau_roles.name', '<>', 'Superadmin');
