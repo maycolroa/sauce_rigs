8b70e28d0010bd21427a1bb744e2e1829b8cb273
Deangerlin Loaiza <yelinloaiza@gmail.com>
2020-08-21 14:48:43 -0400

    ajuste

--- app/Traits/DangerMatrixTrait.php
+++ app/Traits/DangerMatrixTrait.php
@@ -149,7 +149,7 @@ trait DangerMatrixTrait
                     'MENOR' => 'MENOR',
                     'LEVE' => 'LEVE',
                     'GRAVE' => 'GRAVE',
-                    'CATASTRóFICA' => 'CATASTRóFICA'
+                    'CATASTRÓFICA' => 'CATASTRÓFICA'
                 ]
             ];
 
