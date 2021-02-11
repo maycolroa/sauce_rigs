f44a3c12efb8c6ce4ffcc82427b9619ccdb9ae92
roberthriera <robertjoserieraumbria@gmail.com>
2021-02-11 13:09:42 -0400

    correccion reincoporaciones ingeomega

--- app/Http/Controllers/System/CustomerMonitoring/CustomerMonitoringController.php
+++ app/Http/Controllers/System/CustomerMonitoring/CustomerMonitoringController.php
@@ -8,6 +8,7 @@ use App\Vuetable\Facades\Vuetable;
 use App\Models\System\Licenses\License;
 use App\Models\General\Company;
 use App\Models\PreventiveOccupationalMedicine\Reinstatements\Check;
+use App\Models\IndustrialSecure\DangerousConditions\Reports\Report;
 use Carbon\Carbon;
 use Validator;
 use DB;
@@ -77,4 +78,39 @@ class CustomerMonitoringController extends Controller
         return Vuetable::of($companies)
                     ->make();
     }
+
+    /*public function dataDangerousConditions(Request $request)
+    {
+        $now = Carbon::now();
+
+        $reports = Report::select(
+            "sau_reinc_checks.company_id AS company_id",
+            DB::raw("SUM(CASE WHEN YEAR(sau_reinc_checks.created_at) = {$now->year} AND MONTH(sau_reinc_checks.created_at) = {$now->month} THEN 1 ELSE 0 END) AS total_mes"),
+            DB::raw("SUM(CASE WHEN YEAR(sau_reinc_checks.created_at) = {$now->year} THEN 1 ELSE 0 END) AS total_anio")
+        )
+        ->withoutGlobalScopes()
+        ->groupBy('sau_reinc_checks.company_id');  
+        
+        $companies = Company::selectRaw('
+                sau_companies.id AS id,
+                sau_companies.name AS name,
+                MAX(sau_licenses.started_at) AS started_at,
+                MAX(sau_licenses.ended_at) AS ended_at,
+                IFNULL(t.total, 0) AS total,
+                IFNULL(t.total_mes, 0) AS total_mes,
+                IFNULL(t.total_anio, 0) AS total_anio'
+            )
+            ->join('sau_licenses', 'sau_licenses.company_id', 'sau_companies.id')
+            ->join('sau_license_module', 'sau_license_module.license_id', 'sau_licenses.id')
+            ->leftJoin(DB::raw("({$reports->toSql()}) as t"), function ($join) {
+                $join->on("t.company_id", "sau_companies.id");
+            })
+            ->mergeBindings($reports->getQuery())
+            ->whereRaw('? BETWEEN started_at AND ended_at', [date('Y-m-d')])
+            ->where('sau_license_module.module_id', 21)
+            ->groupBy('sau_companies.id');
+
+        return Vuetable::of($companies)
+                    ->make();
+    }*/
 }
