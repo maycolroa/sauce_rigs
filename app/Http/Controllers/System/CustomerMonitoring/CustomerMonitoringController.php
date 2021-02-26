4dcdac0aa2bf093d1cdaefcb2ef1008c0902edf7
roberthriera <robertjoserieraumbria@gmail.com>
2021-02-26 17:27:40 -0400

    SAUC-677 #comment avance informe matriz legal articulos calificados monitoreo

--- app/Http/Controllers/System/CustomerMonitoring/CustomerMonitoringController.php
+++ app/Http/Controllers/System/CustomerMonitoring/CustomerMonitoringController.php
@@ -18,6 +18,7 @@ use App\Models\LegalAspects\Contracts\ContractLesseeInformation;
 use App\Models\LegalAspects\Contracts\ItemQualificationContractDetail;
 use App\Models\LegalAspects\Contracts\EvaluationContract;
 use App\Models\LegalAspects\Contracts\EvaluationFile;
+use App\Models\LegalAspects\LegalMatrix\ArticleFulfillment;
 use Carbon\Carbon;
 use Validator;
 use DB;
@@ -315,6 +316,41 @@ class CustomerMonitoringController extends Controller
             ->mergeBindings($eva_files->getQuery())
             ->whereRaw('? BETWEEN started_at AND ended_at', [date('Y-m-d')])
             ->where('sau_license_module.module_id', 14)
+            ->groupBy('sau_companies.id');
+
+        return Vuetable::of($companies)
+                    ->make();
+    }
+
+    public function dataLegalMatrix(Request $request)
+    {
+        $now = Carbon::now();
+
+        $articles = ArticleFulfillment::select(
+            "sau_lm_articles_fulfillment.company_id AS company_id",
+            DB::raw("SUM(CASE WHEN YEAR(sau_lm_articles_fulfillment.updated_at) = {$now->year} AND MONTH(sau_lm_articles_fulfillment.updated_at) = {$now->month} THEN 1 ELSE 0 END) AS cal_mes"),
+            DB::raw("SUM(CASE WHEN YEAR(sau_lm_articles_fulfillment.updated_at) = {$now->year} THEN 1 ELSE 0 END) AS cal_anio")
+        )
+        ->withoutGlobalScopes()
+        ->where('sau_lm_articles_fulfillment.created_at', '<>', 'sau_lm_articles_fulfillment.updated_at')
+        ->groupBy('sau_lm_articles_fulfillment.company_id');  
+        
+        $companies = Company::selectRaw('
+                sau_companies.id AS id,
+                sau_companies.name AS name,
+                MAX(sau_licenses.started_at) AS started_at,
+                MAX(sau_licenses.ended_at) AS ended_at,
+                IFNULL(t.cal_mes, 0) AS cal_mes,
+                IFNULL(t.cal_anio, 0) AS cal_anio'
+            )
+            ->join('sau_licenses', 'sau_licenses.company_id', 'sau_companies.id')
+            ->join('sau_license_module', 'sau_license_module.license_id', 'sau_licenses.id')
+            ->leftJoin(DB::raw("({$articles->toSql()}) as t"), function ($join) {
+                $join->on("t.company_id", "sau_companies.id");
+            })
+            ->mergeBindings($articles->getQuery())
+            ->whereRaw('? BETWEEN started_at AND ended_at', [date('Y-m-d')])
+            ->where('sau_license_module.module_id', 17)
             ->groupBy('sau_companies.id');
 
         return Vuetable::of($companies)
---
+++ resources/assets/views/System/customerMonitoring/indexLegalMatrix.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <header-module
+      title="SISTEMA"
+      subtitle="MONITOREO DE CLIENTES - MATRÍZ LEGAL"
+      url="system-customermonitoring"
+    />
+
+    <div class="col-md">
+      <b-card no-body>
+        <b-card-body>
+             <vue-table
+                configName="system-customermonitoring-legalMatrix"
+                v-if="auth.can['customerMonitoring_r']"
+                ></vue-table>
+        </b-card-body>
+    </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import Alerts from '@/utils/Alerts.js';
+
+export default {
+  name: 'customermonitoring-legalMatrix',
+  metaInfo: {
+    title: 'Monitoreo de clientes- Matríz legal'
+  }
+}
+</script>
