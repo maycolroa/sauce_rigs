7872db8c8a62062f4e81e3a625a2896add1d7cf2
Roberth Riera <robertjoserieraumbria@gmail.com>
2021-09-07 15:52:00 -0400

    SAUC-833 #comment grafico de radar para evaluaciones audiometrias

---
+++ resources/assets/components/Chartjs/ChartRadar.vue
@@ -0,0 +1,24 @@
+<script>
+import VueChartJs from 'vue-chartjs'
+
+export default {
+    extends: VueChartJs.Radar,
+    props: {
+        chartData: {
+            type: Object,
+            default() {
+                return {
+                    labels: [],
+                    datasets: []
+                }
+            }
+        }
+    },
+    mounted () {
+        this.renderChart(this.chartData, {
+            responsive: false,
+            maintainAspectRatio: false
+        });
+    }
+  }
+</script>
