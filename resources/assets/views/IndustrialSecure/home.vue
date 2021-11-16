21db54803346ad445d890a5bc1a3ec66b9272df3
Roberth Riera <robertjoserieraumbria@gmail.com>
2021-11-16 19:32:38 -0400

    creacion de modulo de epp

--- resources/assets/views/IndustrialSecure/home.vue
+++ resources/assets/views/IndustrialSecure/home.vue
@@ -58,6 +58,27 @@
                     </router-link>
                 </b-col>
             </b-row>
+
+            <div class="row" v-if="checkModule('riskmatrix') && checkModule('riskmatrix')">
+                <div class="col-md-5 offset-md-1" style="padding-top: 40px; padding-bottom: 20px;">
+                    <hr class="border-dark mt-0 mb-4">
+                </div>
+            </div>
+
+            <b-row v-if="checkModule('epp')">
+                <b-col cols="8">
+                    <router-link :to="{ name: 'industrialsecure-dangermatrix-menu'}" v-on:click.native="activityUser('Matríz de peligros')" class="text-dark cursor-pointer item-app-navbar">
+                        <center>
+                            <div style="font-size: 20px;" class="my-2 mx-2 text-center font-weight-bold" ref="epp" @mouseover="changeClassImage('epp', 'epp_hover')">
+                                <img class="ui-w-80" src="/images/contracts.png" alt=""> ELEMENTOS DE PROTECCIÓN PERSONAL
+                            </div>
+                            <div style="font-size: 20px; text-decoration: underline rgb(244, 75, 82); text-underline-position: under;" class="my-2 mx-2 text-center font-weight-bold imgHidden" ref="epp_hover" @mouseleave="changeClassImage('epp_hover', 'epp')">
+                                <img class="ui-w-80" src="/images/contracts_hover.png" alt=""> ELEMENTOS DE PROTECCIÓN PERSONAL
+                            </div>
+                        </center>
+                    </router-link>
+                </b-col>
+            </b-row>
         </template>
     </div>
 </template>
