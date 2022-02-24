fa762ba31c694d53a5ddd7b5a18b180375ae3805
Roberth Riera <robertjoserieraumbria@gmail.com>
2022-02-24 17:01:29 -0400

    ajustes

--- resources/assets/components/IndustrialSecure/Epp/FormElementIncomeComponent.vue
+++ resources/assets/components/IndustrialSecure/Epp/FormElementIncomeComponent.vue
@@ -24,7 +24,7 @@
               <vue-advanced-select v-if="element.type == 'Identificable'" :disabled="viewOnly" class="col-md-12" v-model="element.codes" name="codes" label="Códigos de los elementos" placeholder="Agregue los códigos" :options="[]" :error="form.errorsFor(`elements_id.${index}.codes`)" :multiple="true" :allow-empty="true" :taggable="true" :searchable="true" :limit="50">
               </vue-advanced-select>
 
-              <vue-input v-if="element.type == 'Identificable' && element.type" :disabled="viewOnly" class="col-md-6" v-model="element.expiration_date" label="Máximos dias de uso" type="number" name="expiration_date" :error="form.errorsFor('expiration_date')" placeholder="Máximos dias de uso"></vue-input>
+              <vue-input v-if="element.type == 'Identificable' && element.expired" :disabled="viewOnly" class="col-md-6" v-model="element.expiration_date" label="Máximos dias de uso" type="number" name="expiration_date" :error="form.errorsFor('expiration_date')" placeholder="Máximos dias de uso"></vue-input>
 
               <vue-ajax-advanced-select-tag-unic :disabled="viewOnly" class="col-md-6" v-model="element.reason" name="reason" :error="form.errorsFor(`elements_id.${index}.reason`)" label="Motivo" placeholder="Seleccione el motivo" :url="tagsSReasonDataUrl" :multiple="false" :allowEmpty="true" :taggable="true"></vue-ajax-advanced-select-tag-unic>
             </b-form-row>
@@ -157,7 +157,7 @@ export default {
       axios.post('/industrialSecurity/epp/income/elementInfo', {id: this.form.elements_id[index].id_ele})
         .then(response => {
             this.form.elements_id[index].type = response.data.type;
-            this.form.elements_id[index].expiration_date = response.data.expiration_date
+            this.form.elements_id[index].expired = response.data.expiration_date
         })
         .catch(error => {
             this.isLoading = false;
