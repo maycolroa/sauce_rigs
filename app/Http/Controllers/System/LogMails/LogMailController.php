e9e627689a79b255de5db57642fe90f9db4f2752
Roberth Riera <robertjoserieraumbria@gmail.com>
2023-01-22 20:55:13 -0400

    ajuste log mails reestablecer contraseñas

--- app/Http/Controllers/System/LogMails/LogMailController.php
+++ app/Http/Controllers/System/LogMails/LogMailController.php
@@ -42,26 +42,53 @@ class LogMailController extends Controller
     public function data(Request $request)
     {
         $mails = LogMail::selectRaw(
-                    'sau_log_mails.*,
+                    'sau_log_mails.id,
+                     sau_log_mails.event,
+                     sau_log_mails.subject,
+                     sau_log_mails.created_at,
                      IF(LENGTH(sau_log_mails.recipients) > 50, CONCAT(SUBSTRING(sau_log_mails.recipients, 1, 50), "..."), sau_log_mails.recipients) AS recipients,
                      sau_modules.display_name AS module,
-                     sau_companies.name AS company'
+                     sau_companies.name AS company,
+                     "SI" AS view'
                 )
                 ->join('sau_modules', 'sau_modules.id', 'sau_log_mails.module_id')
                 ->leftJoin('sau_companies', 'sau_companies.id', 'sau_log_mails.company_id');
+
+        $mails2 = DB::table('sent_emails')->selectRaw("
+                    sent_emails.id,
+                    'Restablecer contraseña' AS event,
+                    sent_emails.subject,
+                    sent_emails.created_at,
+                    IF(LENGTH(sent_emails.recipient) > 50, CONCAT(SUBSTRING(sent_emails.recipient, 1, 50), '...'), sent_emails.recipient) AS recipients,
+                    'Usuarios' AS module,
+                    'RIGS' AS company,
+                    'NO' AS view
+                ")
+                ->where('sent_emails.subject', DB::raw("'SAUCE - Restablecer contraseña'"));
+
 
         $url = "/system/logmails";
 
         $filters = COUNT($request->get('filters')) > 0 ? $request->get('filters') : $this->filterDefaultValues($this->user->id, $url);
 
-        if (COUNT($filters) > 0)
-        {
+        if (COUNT($filters['companies']) > 0)
             $mails->inCompanies($this->getValuesForMultiselect($filters["companies"]), $filters['filtersType']['companies']);
+        if (COUNT($filters['modules']) > 0)
             $mails->inModules($this->getValuesForMultiselect($filters["modules"]), $filters['filtersType']['modules']);
+        
+        if (COUNT($filters['companies']) == 0 && COUNT($filters['modules']) == 0)
+        {
+            $mails = $mails->union($mails2);
         }
 
         return Vuetable::of($mails)
-                    ->make();
+            ->addColumn('system-logmails-view', function ($mail) {
+                if ($mail->view == 'SI')
+                    return true;
+                else 
+                    return false;
+            })
+            ->make();
     }
 
     /**
