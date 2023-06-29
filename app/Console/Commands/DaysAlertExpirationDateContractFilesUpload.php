4e847d20c5b38c5d5e07361a8ac981febbe5dfe5
Roberth Riera <robertjoserieraumbria@gmail.com>
2023-06-29 01:50:42 -0400

    ajustes

--- app/Console/Commands/DaysAlertExpirationDateContractFilesUpload.php
+++ app/Console/Commands/DaysAlertExpirationDateContractFilesUpload.php
@@ -6,6 +6,7 @@ use Illuminate\Console\Command;
 use App\Facades\ConfigurationCompany\Facades\ConfigurationsCompany;
 use App\Facades\Mail\Facades\NotificationMail;
 use App\Models\LegalAspects\Contracts\FileUpload;
+use App\Models\LegalAspects\Contracts\ContractLesseeInformation;
 use App\Traits\ContractTrait;
 use Carbon\Carbon;
 
@@ -50,39 +51,110 @@ class DaysAlertExpirationDateContractFilesUpload extends Command
         {
             $company_id = $value['company_id'];
 
-            $files = FileUpload::select(
-                    'sau_ct_file_upload_contracts_leesse.*',
-                    'sau_users.name as user_name'
-                )
-                ->join('sau_users','sau_users.id','sau_ct_file_upload_contracts_leesse.user_id')
-                ->join('sau_ct_file_upload_contract','sau_ct_file_upload_contract.file_upload_id','sau_ct_file_upload_contracts_leesse.id')
-                ->join('sau_ct_information_contract_lessee', 'sau_ct_information_contract_lessee.id', 'sau_ct_file_upload_contract.contract_id')
-                ->whereRaw("CURDATE() = DATE_ADD(sau_ct_file_upload_contracts_leesse.expirationDate, INTERVAL -".$value['value']." DAY)")
-                ->groupBy('sau_ct_file_upload_contracts_leesse.id');
-            
-            $files->company_scope = $company_id;
-            $files = $files->get();
+            $contracts = ContractLesseeInformation::where('company_id', $company_id)
+            ->withoutGlobalScopes()->isActive()->get();
 
-            if ($files->count() > 0)
+            foreach ($contracts as $key => $contract) 
             {
-                $recipients = $this->getUsersMasterContract($company_id);
-
-                $recipients = $recipients->filter(function ($recipient, $index) use ($company_id) {
-                    return $recipient->can('contracts_receive_notifications', $company_id) && !$recipient->isSuperAdmin($company_id);
-                });
-
-                if (!$recipients->isEmpty())
+                if ($value['value'])
                 {
-                    NotificationMail::
-                        subject('Contratistas - Archivos Próximos a Vencerse')
-                        ->view('LegalAspects.contract.filesUploadExpiration')
-                        ->recipients($recipients)
-                        ->message('Los siguientes archivos están próximos a vencerse: ')
-                        ->module('contracts')
-                        ->event('Tarea programada: DaysAlertExpirationDateContractFilesUpload')
-                        ->table($this->prepareDataTable($files))
-                        ->company($company_id)
-                        ->send();
+                    $files_contracts = FileUpload::select(
+                            'sau_ct_file_upload_contracts_leesse.name as name',
+                            'sau_ct_file_upload_contracts_leesse.expirationDate as expirationDate',
+                            'sau_ct_file_upload_contracts_leesse.created_at as created_at',
+                            'sau_ct_file_upload_contracts_leesse.updated_at as updated_at',
+                            'sau_users.name as user_name',
+                            'sau_ct_information_contract_lessee.id as contract_id'
+                        )
+                        ->withoutGlobalScopes()
+                        ->join('sau_users','sau_users.id','sau_ct_file_upload_contracts_leesse.user_id')
+                        ->join('sau_ct_file_upload_contract','sau_ct_file_upload_contract.file_upload_id','sau_ct_file_upload_contracts_leesse.id')
+                        ->join('sau_ct_information_contract_lessee', 'sau_ct_information_contract_lessee.id', 'sau_ct_file_upload_contract.contract_id')
+                        ->whereRaw("CURDATE() = DATE_ADD(sau_ct_file_upload_contracts_leesse.expirationDate, INTERVAL -".$value['value']." DAY)")
+                        ->where('sau_ct_information_contract_lessee.id', $contract->id)
+                        ->groupBy('sau_ct_file_upload_contracts_leesse.id', 'sau_ct_information_contract_lessee.id');
+
+                    $files_globals = FileUpload::select(
+                            'sau_ct_file_upload_contracts_leesse.name as name',
+                            'sau_ct_file_upload_contracts_leesse.expirationDate as expirationDate',
+                            'sau_ct_file_upload_contracts_leesse.created_at as created_at',
+                            'sau_ct_file_upload_contracts_leesse.updated_at as updated_at',
+                            'sau_users.name as user_name',
+                            'sau_ct_file_document_contract.contract_id as contract_id'
+                        )
+                        ->withoutGlobalScopes()
+                        ->join('sau_users','sau_users.id','sau_ct_file_upload_contracts_leesse.user_id')
+                        ->join('sau_ct_file_document_contract','sau_ct_file_document_contract.file_id','sau_ct_file_upload_contracts_leesse.id')
+                        ->whereRaw("CURDATE() = DATE_ADD(sau_ct_file_upload_contracts_leesse.expirationDate, INTERVAL -".$value['value']." DAY)")
+                        ->where('sau_ct_file_document_contract.contract_id', $contract->id)
+                        ->groupBy('sau_ct_file_upload_contracts_leesse.id', 'sau_ct_file_document_contract.contract_id');
+                    
+                    $files_employees = FileUpload::select(
+                            'sau_ct_file_upload_contracts_leesse.name as name',
+                            'sau_ct_file_upload_contracts_leesse.expirationDate as expirationDate',
+                            'sau_ct_file_upload_contracts_leesse.created_at as created_at',
+                            'sau_ct_file_upload_contracts_leesse.updated_at as updated_at',
+                            'sau_users.name as user_name',
+                            'sau_ct_contract_employees.contract_id as contract_id'
+                        )
+                        ->withoutGlobalScopes()
+                        ->join('sau_users','sau_users.id','sau_ct_file_upload_contracts_leesse.user_id')
+                        ->join('sau_ct_file_document_employee','sau_ct_file_document_employee.file_id','sau_ct_file_upload_contracts_leesse.id')
+                        ->join('sau_ct_contract_employees','sau_ct_contract_employees.id','sau_ct_file_document_employee.employee_id')
+                        ->whereRaw("CURDATE() = DATE_ADD(sau_ct_file_upload_contracts_leesse.expirationDate, INTERVAL -".$value['value']." DAY)")
+                        ->where('sau_ct_contract_employees.contract_id', $contract->id)
+                        ->groupBy('sau_ct_file_upload_contracts_leesse.id', 'sau_ct_contract_employees.contract_id');
+
+                    $files_list_check = FileUpload::select(
+                            'sau_ct_file_upload_contracts_leesse.name as name',
+                            'sau_ct_file_upload_contracts_leesse.expirationDate as expirationDate',
+                            'sau_ct_file_upload_contracts_leesse.created_at as created_at',
+                            'sau_ct_file_upload_contracts_leesse.updated_at as updated_at',
+                            'sau_users.name as user_name',
+                            'sau_ct_list_check_qualifications.contract_id as contract_id'
+                        )
+                        ->withoutGlobalScopes()
+                        ->join('sau_users','sau_users.id','sau_ct_file_upload_contracts_leesse.user_id')
+                        ->join('sau_ct_file_item_contract','sau_ct_file_item_contract.file_id','sau_ct_file_upload_contracts_leesse.id')
+                        ->join('sau_ct_list_check_qualifications','sau_ct_list_check_qualifications.id','sau_ct_file_item_contract.list_qualification_id')
+                        ->whereRaw("CURDATE() = DATE_ADD(sau_ct_file_upload_contracts_leesse.expirationDate, INTERVAL -".$value['value']." DAY)")
+                        ->where('sau_ct_list_check_qualifications.contract_id', $contract->id)
+                        ->groupBy('sau_ct_file_upload_contracts_leesse.id', 'sau_ct_list_check_qualifications.contract_id');
+
+                    $files_contracts->union($files_globals);
+                    $files_contracts->union($files_employees);
+                    $files_contracts->union($files_list_check);
+                    $files_contracts = $files_contracts->get();
+
+                    if ($files_contracts->count() > 0)
+                    {
+                        $recipients = $this->getUsersContract($contract->id, $company_id);
+
+                        $responsibles = $contract->responsibles;
+
+                        foreach ($responsibles as $key => $value) 
+                        {
+                            $recipients->push($value);
+                        }
+
+                        $recipients = $recipients->filter(function ($recipient, $index) use ($company_id) {
+                            return $recipient->can('contracts_receive_notifications', $company_id) && !$recipient->isSuperAdmin($company_id);
+                        });
+
+                        if (!$recipients->isEmpty())
+                        {
+                            NotificationMail::
+                                subject('Contratistas - Archivos Próximos a Vencerse')
+                                ->view('LegalAspects.contract.filesUploadExpiration')
+                                ->recipients($recipients)
+                                ->message('Los siguientes archivos están próximos a vencerse: ')
+                                ->module('contracts')
+                                ->event('Tarea programada: DaysAlertExpirationDateContractFilesUpload')
+                                ->table($this->prepareDataTable($files_contracts))
+                                ->company($company_id)
+                                ->send();
+                        }
+                    }
                 }
             }
         }
