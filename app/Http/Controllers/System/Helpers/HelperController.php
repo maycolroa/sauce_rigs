04e76b3283e4f39c7e51bc508691de5fba2c995e
Roberth Riera <robertjoserieraumbria@gmail.com>
2023-12-18 23:11:01 -0400

    ajustes

--- app/Http/Controllers/System/Helpers/HelperController.php
+++ app/Http/Controllers/System/Helpers/HelperController.php
@@ -227,8 +227,8 @@ class HelperController extends Controller
                 {
                     $file_tmp = $value['file'];
                     $nameFile = base64_encode($this->user->id . now() . rand(1,10000) . $keyF) .'.'. $file_tmp->extension();
-                    Storage::disk('s3')->put('system/helpers/files/'. $nameFile, $file_tmp, 'public');
-                    //$file_tmp->storeAs($fileUpload->path_client(false), $nameFile, 's3');
+                    //Storage::disk('s3')->put('system/helpers/files/'. $nameFile, $file_tmp, 'public');
+                    $file_tmp->storeAs($fileUpload->path_client(false), $nameFile, 's3');
                     $fileUpload->file = $nameFile;
                 }
 
