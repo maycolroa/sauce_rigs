5abe5ee4b1cc5a94e7791885cecbec162e0c35f0
Roberth Riera <robertjoserieraumbria@gmail.com>
2024-04-09 10:13:24 -0400

    ajuste

--- resources/assets/views/IndustrialSecure/roadSafety/indexMenu.vue
+++ resources/assets/views/IndustrialSecure/roadSafety/indexMenu.vue
@@ -43,11 +43,13 @@
 								</router-link>
 							</div>
 
-							<div class="d-flex col-sm-6 col-md-4 c ol-lg-6 align-items-center">
-								<router-link tag="a" href="javascript:void(0)" class="card-body media align-items-center text-dark" :to="{name:'industrialsecure-roadsafety-customHelpers'}"  >
-									<i class="ion ion-md-settings display-4 d-block text-primary"></i>
+							<div class="d-flex col-sm-6 col-md-6 col-lg-6 align-items-center" v-if="auth.can['roadsafety_trainings_r']">
+								<router-link tag="a" href="javascript:void(0)" class="card-body media align-items-center text-dark" :to="{name:'industrialsecure-roadsafety-trainings'}">
+									<i class="lnr lnr-book display-4 d-block text-primary"></i>
 									<span class="media-body d-block ml-3">
-										<span class="text-big font-weight-bolder">Ayudas</span>
+										<span class="text-big font-weight-bolder">Capacitaciones</span>
+										<br>
+										<small class="text-muted">Administrar Capacitaciones</small>
 									</span>
 								</router-link>
 							</div>
@@ -63,13 +65,11 @@
 								</router-link>
 							</div>
 
-							<div class="d-flex col-sm-6 col-md-6 col-lg-6 align-items-center" v-if="auth.can['roadsafety_trainings_r']">
-								<router-link tag="a" href="javascript:void(0)" class="card-body media align-items-center text-dark" :to="{name:'roadsafety-trainings'}">
-									<i class="lnr lnr-book display-4 d-block text-primary"></i>
+							<div class="d-flex col-sm-6 col-md-4 c ol-lg-6 align-items-center">
+								<router-link tag="a" href="javascript:void(0)" class="card-body media align-items-center text-dark" :to="{name:'industrialsecure-roadsafety-customHelpers'}"  >
+									<i class="ion ion-md-settings display-4 d-block text-primary"></i>
 									<span class="media-body d-block ml-3">
-										<span class="text-big font-weight-bolder">Capacitaciones</span>
-										<br>
-										<small class="text-muted">Administrar Capacitaciones</small>
+										<span class="text-big font-weight-bolder">Ayudas</span>
 									</span>
 								</router-link>
 							</div>
