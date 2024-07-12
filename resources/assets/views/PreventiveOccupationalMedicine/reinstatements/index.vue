4cd297ea9cefa04a241155d213afc669192ca504
Roberth Riera <robertjoserieraumbria@gmail.com>
2024-07-12 13:13:15 -0400

    ajuste

--- resources/assets/views/PreventiveOccupationalMedicine/reinstatements/index.vue
+++ resources/assets/views/PreventiveOccupationalMedicine/reinstatements/index.vue
@@ -98,7 +98,7 @@
 								</router-link>
 							</div>
 
-							<div class="d-flex col-sm-6 col-md-4 col-lg-6 align-items-center" v-if="auth.can['reinc_checks_r']">
+							<div class="d-flex col-sm-6 col-md-4 col-lg-6 align-items-center" v-if="auth.can['reinc_checks_c']">
 								<router-link tag="a" href="javascript:void(0)" class="card-body media align-items-center text-dark" :to="{name:'reinstatements-customHelpers'}">
 									<i class="ion ion-md-settings display-4 d-block text-primary"></i>
 									<span class="media-body d-block ml-3">
