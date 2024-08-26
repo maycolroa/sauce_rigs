e790a984b09081b7c8cda88a3d28d6a381288d19
Roberth Riera <robertjoserieraumbria@gmail.com>
2024-08-26 11:18:04 -0400

    ajuste

--- app/Http/Controllers/LegalAspects/Contracs/ContractEmployeeController.php
+++ app/Http/Controllers/LegalAspects/Contracs/ContractEmployeeController.php
@@ -909,7 +909,7 @@ class ContractEmployeeController extends Controller
                     $apply_motive = '';
                     $files->transform(function($file, $index) use (&$apply_file, &$apply_motive){
 
-                        $type = explode('.',$file->file)[1];
+                        $type = $file->file ? explode('.',$file->file)[1] : null;
                         $file->key = Carbon::now()->timestamp + rand(1,10000);
                         $file->old_name = $file->file;
                         $file->type = $type;
