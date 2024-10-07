493caba4dab9b729b640f88f9a1e3b482852d309
Roberth Riera <robertjoserieraumbria@gmail.com>
2024-10-07 15:08:24 -0400

    ajuste

--- app/Traits/UtilsTrait.php
+++ app/Traits/UtilsTrait.php
@@ -231,8 +231,10 @@ trait UtilsTrait
 
     protected function getValuesForMultiselectTags($data, $keyRef = 'value')
     {
+        \Log::info($data);
         return  collect($data)
                 ->transform(function ($item, $index) use ($keyRef) {
+                    \Log::info($item);
                     return trim(str_replace(',', '', $item[$keyRef]));
                 })
                 ->filter(function ($item, $key) {
