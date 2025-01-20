c471fa2b6683112e1a7be8696fc48e457f52f0e8
Roberth Riera <robertjoserieraumbria@gmail.com>
2025-01-19 23:19:44 -0400

    ajustes

--- resources/assets/components/Administrative/Auth/PasswordResetHelperComponent.vue
+++ resources/assets/components/Administrative/Auth/PasswordResetHelperComponent.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
+    <img style="width: 400px; height: 100px; margin-left: 38%; margin-top: 2%" src="~@/images/Sauce-ML Logo RiGS Principal.png">
+    <hr class="border-dark mt-0 mb-4" style="margin-left: 30%; margin-top: 2%; margin-right: 30%">
     <b-row>
-      <video style="width: 70%; height: 600px; margin-left: 15%; margin-top: 5%" controls>
+      <video style="width: 50%; height: 45%; margin-left: 25%;" controls>
         <source src="https://appsauce.s3.us-east-1.amazonaws.com/system/helpers/files/1/Restablecimiento+de+contrase%C3%B1a+(1).mp4" type="video/mp4">
       </video>
     </b-row>
-    <b-row style="margin-left: 70%; margin-top: 2%">
+    <b-row style="margin-left: 40%; margin-top: 2%; margin-bottom: 3%">
       <div>
-          <b-btn variant="default" :to="cancelUrl">Atras</b-btn>&nbsp;&nbsp;
-          <b-btn variant="primary" :to="passwordResetAction">Ir a "Olvidaste tu contraseña"</b-btn>&nbsp;&nbsp;
+          <b-btn variant="default" :to="cancelUrl">Iniciar sesión</b-btn>&nbsp;&nbsp;
+          <b-btn variant="primary" :to="passwordResetAction">Ir a "¿Olvidaste tu contraseña?"</b-btn>&nbsp;&nbsp;
       </div>
     </b-row>
   </div>
