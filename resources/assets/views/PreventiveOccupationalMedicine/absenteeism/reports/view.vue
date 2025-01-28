f88a0884ff4cdc58b7fc666f8c82b3be5b34c0c5
Roberth Riera <robertjoserieraumbria@gmail.com>
2025-01-28 17:14:49 -0400

    ajuste

--- resources/assets/views/PreventiveOccupationalMedicine/absenteeism/reports/view.vue
+++ resources/assets/views/PreventiveOccupationalMedicine/absenteeism/reports/view.vue
@@ -8,9 +8,9 @@
 
     <div class="col-md">
       <b-card no-body>
-        <b-card-body>
+        <b-card-body style="height: 850px; width: 1000px;">
           <perfect-scrollbar :options="{ wheelPropagation: true }" class="mb-4" style="height: 450px; width: 1200px; padding-right: 10px;">
-            <iframe :src="data.url" class="col-md-12" height="800" width="1800" frameborder="0" id="iframeInforme"></iframe>
+            <iframe :src="data.url" class="col-md-12" height="800" frameborder="0" id="iframeInforme"></iframe>
           </perfect-scrollbar>
         </b-card-body>
       </b-card>
