a6e46eb79ae399f62d8be78dd12f7fe8d9c1b00d
Roberth Riera <robertjoserieraumbria@gmail.com>
2025-10-23 17:05:15 -0400

    ajuste

--- resources/views/pdf/letterDeliveryEppNew.blade.php
+++ resources/views/pdf/letterDeliveryEppNew.blade.php
@@ -63,7 +63,7 @@
                 <tr>
                     <th colspan="3">                        
                         @if ($delivery->logo)
-                        <div style="text-align: left"><img src="{{ $delivery->logo }}" width="40%" height="90%"/></div>
+                        <div style="text-align: left"><img src="{{ $delivery->logo }}" width="35%" height="80%"/></div>
                         @endif
                     </th>
                 </tr>
