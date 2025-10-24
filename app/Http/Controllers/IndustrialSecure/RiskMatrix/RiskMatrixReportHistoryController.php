7d0575767466d25326b048a49d45c4fbf32bdf25
Roberth Riera <robertjoserieraumbria@gmail.com>
2025-10-23 23:18:05 -0400

    Ajustes matriz de riesgos

--- app/Http/Controllers/IndustrialSecure/RiskMatrix/RiskMatrixReportHistoryController.php
+++ app/Http/Controllers/IndustrialSecure/RiskMatrix/RiskMatrixReportHistoryController.php
@@ -101,6 +101,7 @@ class RiskMatrixReportHistoryController extends Controller
         $company = Company::select('logo')->where('id', $this->company)->first();
 
         $logo = ($company && $company->logo) ? $company->logo : null;
+        $logo = $logo ? Storage::disk('s3')->url('administrative/logos/'.$logo) : null;
         
         $data = [
             'inherent_report' => RiskMatrixHistoryManager::reportInherent($request, $request->filters, $this->user->id, $this->company),
